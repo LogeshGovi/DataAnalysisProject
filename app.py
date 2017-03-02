7c4b9aadd16c63ac73a85858fb72987c1a5530cf
logesh.govindarajulu@student.fh-kiel.de <logesh.govindarajulu@student.fh-kiel.de>
2017-03-02 16:45:58 +0100

    central values of a distribution

--- app.py
+++ app.py
@@ -8,17 +8,19 @@ import numpy as np
 from datamanip.DataDeDuplication import Deduplication as dd
 from datamanip.CentralValues import CentralValues as cv
 
-#ddf = dd.dataDedup_csv("D:\\fulldataLearningContext.csv")
-#ddf1, ddf2, ddf3 = np.array_split(ddf,3)
-
-#df_arr = [ddf1, ddf2, ddf3]
-#for arr in df_arr:
-#    arr.to_csv("D:\\ddf"+str(i+1)+".csv",encoding='utf-8', index=False,
-#                         header=False)
-#    i = i+1
+ddf = dd.dataDedup_csv("D:\\fulldataLearningContext.csv")
+split_ddf = dd.dataFrameSplit(ddf,)
+"""
+ddf1, ddf2, ddf3 = np.array_split(ddf,3)
+i=0
+df_arr = [ddf1, ddf2, ddf3]
+for arr in df_arr:
+    arr.to_csv("D:\\ddf"+str(i+1)+".csv",encoding='utf-8', index=False,
+                         header=False)
+    i = i+1
     
     
 CenVal = cv((np.arange(1,101)))
 cVal = CenVal.cenvalues()
 print(cVal)
-
+"""
--- datamanip/DataDeDuplication.py
+++ datamanip/DataDeDuplication.py
@@ -4,6 +4,7 @@ Created on Mon Feb 27 12:44:04 2017
 
 @author: Logesh Govindarajulu
 """
+import numpy as np
 import pandas as pd
 import  os.path as fpath
 
@@ -39,3 +40,41 @@ class Deduplication:
             
         else:
             print("file \"%s\" does not exist... or is not a file..." %(infile))
+
+
+
+
+    # This is a function to split the dataframes such that each frame contains 
+    # approximately 1 million records
+    def dataFrameSplit(df, norec=1000000, outfile= None):
+        """
+    This function checks for the size of a dataframe and splits it into parts 
+    containing approximately 1 million records as the default number of records
+    for each dataframe.It also provides the option of writing the split 
+    dataframes to the disk.
+        
+          ** Parameters** , **type**, **return values**
+          :param df : dataframe to be split
+          :param norec: number of records needed for each split *default 1000000*
+          :type  arg1: pandas dataframe
+          :type arg2: integer
+          :return: returns an array of dataframes
+    """
+        # calculation of the no. of rows of the dataframe
+        df_rsz = len(df.index)
+        if df_rsz>norec:
+            no_splits = np.ceil(df_rsz/norec)
+            dfarr = np.array_split(df,no_splits)
+            return dfarr
+        else:
+            print("The dataframe doesn't have sufficient records")
+            
+        # printing to disk when     
+        if outfile!=None:
+            i=0
+            for arr in dfarr:
+                arr.to_csv("D:\\ddf"+str(i+1)+".csv",encoding='utf-8', index=False,
+                                     header=False)
+                i = i+1
+                    
+        
