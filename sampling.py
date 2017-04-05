77ff483d2a109f32c24dd73648e14c4542e4ed13
logesh.govindarajulu@student.fh-kiel.de <logesh.govindarajulu@student.fh-kiel.de>
2017-04-05 14:17:03 +0200

    groundwork for random sampling done

---
+++ sampling.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  5 12:37:06 2017
+
+@author: Logesh Govindarajulu
+"""
+
+import numpy as np
+from random import randint
+a = set()
+c=[]
+k = 0
+x = 170800
+
+while len(a)<np.int(x):
+    b=randint(1,170800)
+    a.add(b)
+    
+for j in a:
+    c.append(j)
+    
+c.sort()
+
