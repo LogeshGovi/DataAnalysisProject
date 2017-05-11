44f9690fd6491f9c6c682c375c96314c8bc75e07
logesh.govindarajulu@student.fh-kiel.de <logesh.govindarajulu@student.fh-kiel.de>
2017-05-11 16:13:30 +0200

    random sampling numpy added

--- anapy/sampling/RandomSampling.py
+++ anapy/sampling/RandomSampling.py
@@ -8,8 +8,11 @@ Created on Wed Apr  5 12:37:06 2017
 import numpy as np
 
 class RandomSampling:
-    
-    def without_replacement(self,df_size, per_sample ):
+
+##########################################################################################################
+# Random Sampling for pandas dataframes
+##########################################################################################################
+    def without_replacement_df(df_size, per_sample ):
         np.random.seed(15)
         df_key = np.array(np.arange(1,df_size))
         no_of_samples = 100//per_sample
@@ -21,17 +24,56 @@ class RandomSampling:
             df_key = np.setdiff1d(df_key,sample)
             samples.append(sample)
         return samples
-                                   
 
-    def with_replacement(self,df_size,per_sample,no_of_samples=10):
+
+    def with_replacement_df(df_size,per_sample,no_of_samples=10):
         np.random.seed(15)
         df_key = np.array(np.arange(1,df_size))
         no_of_observations = (df_size*per_sample)//100
         samples=[]
         for i in range(no_of_samples):
-            sample= np.random.choice(df_key, size = no_of_observations, 
+            sample= np.random.choice(df_key, size = no_of_observations,
                                      replace=False)
-            samples.append(sample) 
+            samples.append(sample)
         return samples
-                                     
-                                     
+
+##########################################################################################################
+
+
+
+##########################################################################################################
+# Random Sampling for numpy datasets
+##########################################################################################################
+
+    def get_random_samples_np(np_array, per_sample, replace = True):
+        """
+        This method takes a two dimensional numpy array and returns a random sample based
+        on the percentage of the samples needed.
+        :param per_sample: 0 - 100
+        :param replace: True or False
+        :return: numpy sample array
+        """
+        replacement = replace
+        def with_replacement_np(np_array, per_sample):
+            np.random.seed(15)
+            # number of samples that are needed to be drawn
+            no_of_observations = len(np_array)*per_sample//100
+            # the row indices that are drawn as sample
+            sample_rows = np.random.choice(np.arange(len(np_array)),no_of_observations,replace=True)
+            sample = np_array[sample_rows,:]
+            return sample
+
+
+        def without_replacement_np(np_array, per_sample):
+            np.random.seed(15)
+            np.random.shuffle(np_array)
+            #number of samples that are needed to be drawn
+            no_of_observations = len(np_array)*per_sample//100
+            sample = np_array[:no_of_observations,:]
+            return sample
+
+        if replacement == True:
+           return list(with_replacement_np(np_array,per_sample))
+        elif replacement == False:
+           return list(without_replacement_np(np_array,per_sample))
+##########################################################################################################
