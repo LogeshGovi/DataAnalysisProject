db3cb7b7d5821a962ed760ad67999692d6259b13
logesh.govindarajulu@student.fh-kiel.de <logesh.govindarajulu@student.fh-kiel.de>
2017-05-18 07:07:13 +0200

    kneighbors tested

--- scripts/RandomSampling/nneighbors.py
+++ scripts/RandomSampling/nneighbors.py
@@ -27,6 +27,7 @@ with open(scaler_file,mode='rb') as h:
 folder_to_write = "D:\\lcif\\16032017-IndividualFiles\\TrainTestdataset\\kneighbors\\"
 sample_sizes = [30,40,50,60,70,80,90,100]
 sampling_methods = ['random', 'systematic','stratified', 'cluster']
+#sampling_methods =['systematic']
 kneighbor = KNeighborsClassifier(n_neighbors=5)
 
 for k,i in enumerate(sampling_methods):
@@ -36,7 +37,7 @@ for k,i in enumerate(sampling_methods):
     time_predict_values = []
     for j in sample_sizes:
         train, test, time = lfs.learn_from_sample(train_data, train_target, 
-                                            test_data, test_target, 30,
+                                            test_data, test_target, j,
                                             i, kneighbor,data_scaler,False,5)
         train_acc = accuracy_score(train[0], train[1])
         test_acc = accuracy_score(test[0], test[1])
@@ -57,14 +58,14 @@ for k,i in enumerate(sampling_methods):
         time_fit_values.append(time[0])
         time_predict_values.append(time[1])
         
-        
-    acc_time_values = np.concatenate((train_acc_values,
-                                      test_acc_values,time_fit_values,
-                                      time_predict_values),axis=0).reshape(4,8).T
-    acc_time_values_df = pd.DataFrame(acc_time_values,index=sample_sizes,columns=['tracc','teacc','fittm','predtm'])
-    acc_time_values_df.to_html(folder_to_write+"acc_time_"+i+".html")
+    if write_to_file == True:
+        acc_time_values = np.concatenate((train_acc_values,
+                                          test_acc_values,time_fit_values,
+                                          time_predict_values),axis=0).reshape(4,8).T
+        acc_time_values_df = pd.DataFrame(acc_time_values,index=sample_sizes,columns=['tracc','teacc','fittm','predtm'])
+        acc_time_values_df.to_html(folder_to_write+"acc_time_"+i+".html")
 
-"""
+
     plt.figure(k+1, figsize=(8,2))
     plt.plot(sample_sizes,train_acc_values,'go',sample_sizes,train_acc_values, 'g' )
     plt.plot(sample_sizes,test_acc_values,'ro',sample_sizes,test_acc_values, 'r')
@@ -72,9 +73,9 @@ for k,i in enumerate(sampling_methods):
     plt.title("Training/Test Accuracy graph for sampling method: "+ i)
     plt.show()
     
-    plt.figure(5)
-    plt.plot(sample_sizes,time_fit_values,'o', sample_sizes,time_fit_values)
-    plt.plot(sample_sizes,time_predict_values,'o', sample_sizes,time_predict_values)
+    plt.figure(k+5)
+    plt.plot(sample_sizes,time_fit_values,'go', sample_sizes,time_fit_values,'k')
+    plt.plot(sample_sizes,time_predict_values,'ro', sample_sizes,time_predict_values,'k')
     plt.title("Fit/Predict graph: ")
     plt.show()
     
@@ -87,7 +88,6 @@ for k,i in enumerate(sampling_methods):
 
     
 
-"""
 
 
 
